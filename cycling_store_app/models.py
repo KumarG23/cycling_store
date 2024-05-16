e0c2b7a88acba11244089a202520a2a10b94686a
Neal Sharma <nealsharma@Neals-MBP.localdomain>
2024-05-16 14:37:50 -0400

    pretty much done

--- cycling_store_app/models.py
+++ cycling_store_app/models.py
@@ -2,9 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+class Handlebar(models.Model):
+    name = models.TextField()
+    handlebars_in_stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Vehicle(models.Model):
     type = models.TextField()
     number_in_stock = models.PositiveIntegerField()
+    handlebar = models.ForeignKey(Handlebar, on_delete=models.SET_NULL, null=True, blank=True)
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f'type: {self.type}, stock: {self.number_in_stock}'
@@ -20,6 +35,8 @@ class Customer(models.Model):
 class CustomerOrder(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order = models.ForeignKey(Vehicle, on_delete=models.SET_NULL, null=True)
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True)
+    handlebar = models.ForeignKey(Handlebar, on_delete=models.SET_NULL, null=True)
     created_date= models.DateField()
     paid = models.BooleanField(default=False)
 
